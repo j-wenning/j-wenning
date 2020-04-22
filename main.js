02b86524e0183c8c6de095828881bdc2092c9250
justin-wenning <wenning.justin@gmail.com>
2020-04-21 20:17:39 -0700

    Added code to open mobile menu Removed scraper prevention in favor of encrypted formsubmit supplied link

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
-const form = document.querySelector("#mailForm");
-const antiScrape = ["2.", "1wenning", "5gmail", "7com", "3justin", "6.", "4@"]
-                      .sort((a, b) => parseInt(a) - parseInt(b))
-                      .map(item => item.substr(1))
-                      .join('');
-form.setAttribute("action", form.getAttribute("action") + antiScrape);
+const aside = document.querySelector('aside');
+const menuButton = document.querySelector('.menu-toggle');
+const animDelay = 500;
+const resizeEvent = { e: window.document.createEvent('UIEvents') };
+let toggled = false;
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth > 425) aside.classList.remove('collapsed');
+  else aside.classList.add('collapsed');
+});
+
+menuButton.addEventListener('click', toggleAside);
+
+aside.addEventListener('click', e => {
+  if (toggled && e.target === e.currentTarget) toggleAside();
+});
+
+function toggleAside() {
+  toggled = !toggled;
+  aside.classList.toggle('collapsed');
+  aside.classList.add('animating');
+  setTimeout(() => {
+    aside.classList.remove('animating');
+  }, animDelay);
+}
+
+resizeEvent.e.initUIEvent('resize', true, false, window, 0);
+window.dispatchEvent(resizeEvent.e);
+delete resizeEvent.e;
