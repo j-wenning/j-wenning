3f4d8418c869f4f2fc30dbd30f5c2919bada4c16
j-wenning <wenning.justin@gmail.com>
2020-10-07 04:04:07 -0700

    Created room manager script

---
+++ server/room-manager.js
@@ -0,0 +1,16 @@
+module.exports = class RoomManager {
+    constructor() {
+        this.rooms = [];
+    }
+    createRoom(id, author = 'anonymous') {
+        const index = this.rooms.length;
+        this.rooms.push({ index, id, author, isActive: true });
+        return this.rooms[index];
+    }
+    getRoom(index) { return this.rooms[index]; }
+    getRooms() { return this.rooms; }
+    getRoomCount() { return this.rooms.length; }
+    updateRoomAuthor(index, author) { this.rooms[index].author = author; }
+    deleteRoom(index) { this.rooms[index].isActive = false; }
+    refreshRooms() { this.rooms = this.rooms.filter(room => room.isActive).map((room, index) => room.index = index) }
+};
