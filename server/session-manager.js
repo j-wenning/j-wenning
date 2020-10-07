2e43089012aaccf13ab408510032fa4a684c583a
j-wenning <wenning.justin@gmail.com>
2020-10-07 04:04:25 -0700

    Created session manager script

---
+++ server/session-manager.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const path = require('path');
+
+const readFile = () => JSON.parse(fs.readFileSync(path.join(__dirname, 'session', 'sessions.json'), { encoding: 'utf-8' }));
+const initFile = () => fs.writeFileSync(path.join(__dirname, 'session', 'sessions.json'), '{}', { flag: 'wx+' });
+const saveFile = data => fs.writeFileSync(path.join(__dirname, 'session', 'sessions.json'), data);
+const randStr = () => Buffer.from(Math.floor(Math.random() * Number.MAX_VALUE).toString()).toString('base64');
+
+class SessionManager {
+    constructor() {
+        try {
+            this.sessions = readFile();
+        } catch(err) {
+            if (err.code === 'ENOENT') {
+                initFile();
+                this.sessions = readFile();
+            }
+        }
+        global.setInterval(() => {
+            const time = Date.now();
+            for(const id in this.sessions) {
+                if (this.getSession(id).expiry < time) {
+                    this.deleteSession(id);
+                }
+            }
+        }, parseInt(process.env.SESSION_REFRESH_INTERVAL));
+        process.on('exit', () => this.saveSessions());
+    }
+    saveSessions() { 
+        console.log('Caching sessions...');
+        saveFile(JSON.stringify(this.sessions));
+        console.log('Cache complete.');
+    }
+    createSession(data = {}) {
+        let id;
+        do id = randStr();
+        while (this.sessions[id]);
+        this.sessions[id] = data;
+        this.sessions[id].expiry = Date.now() + parseInt(process.env.SESSION_EXPIRY);
+        return id;
+    }
+    getSession(id) { return id ? this.sessions[id] || {} : {}; }
+    updateSession(id, data) { Object.assign(this.sessions[id], data); }
+    deleteSession(id) { delete this.sessions[id]; }
+};
+
+module.exports = new SessionManager();
